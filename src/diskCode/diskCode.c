c0646d5195861db0d3b4fcb4a109653115ed0bac
skawo <43761362+skawo@users.noreply.github.com>
2025-12-21 21:03:17 +0100

    Interrupts

--- src/diskCode/diskCode.c
+++ src/diskCode/diskCode.c
@@ -8,7 +8,7 @@ extern void* __FileSystemStart;
 static resolution_t res = RESOLUTION_320x240;
 static bitdepth_t bit = DEPTH_16_BPP;
 
-extern void *  VI_callback;
+extern void* inthandler;
 extern int __boot_memsize;
 
 extern void __init_interrupts();
@@ -18,7 +18,24 @@ extern void __rdpq_paragraph_char_check_bitfield();
 extern void display_show_force(display_context_t dc);
 
 void Disk_Main()
-{   
+{
+    u32* intHandlerEntry1 = (u32*)0xA0000000;
+    u32* intHandlerEntry2 = (u32*)0xA0000080;
+    u32* intHandlerEntry3 = (u32*)0xA0000100;
+    u32* intHandlerEntry4 = (u32*)0xA0000180;
+
+    u32 jInstr = 0x08000000 | (((u32)&inthandler >> 2) & 0x03FFFFF);
+
+    *intHandlerEntry1 = jInstr;
+    *intHandlerEntry2 = jInstr;
+    *intHandlerEntry3 = jInstr;
+    *intHandlerEntry4 = jInstr;
+
+    __init_interrupts();
+    __joybus_init();
+    __inspector_init();
+    __rdpq_paragraph_char_check_bitfield();
+
     debug_init_isviewer();
 
     display_close();
@@ -26,35 +43,36 @@ void Disk_Main()
 
     graphics_set_default_font();
 
+    joypad_init();
+
     debugf("TEST2\n");
 
     int x = 20;
     int y = 20;
-    int vx = 1; 
-    int vy = 2;
 
     while (1)
     {
         display_context_t disp = 0;
         disp = display_get();
 
-        debugf("disp: %x\n", disp);
-
         graphics_fill_screen( disp, 0 );
-        graphics_draw_text( disp, x, y, "HELLO WORLD" );
-        
-        display_show_force(disp);
+        //graphics_draw_text( disp, x, y, "HELLO WORLD" );
 
-        x += vx;
-        y += vy;
+        sprite_t* sp = (sprite_t*)FILE_OFFS(MARIOSPRITE_BIN); 
+        graphics_draw_sprite( disp, x, y, sp );
 
-        if (x < 0 || x > display_get_width() - 8 * 11)
-            vx = -vx;
-        if (y < 0 || y > display_get_height() - 8)
-            vy = -vy;
+        display_show(disp);
 
-        wait_ms(10);
+        joypad_poll();
+        joypad_buttons_t keys = joypad_get_buttons_held(JOYPAD_PORT_1);
 
-
+        if (keys.d_up)
+            y--;
+        if (keys.d_down)
+            y++;            
+        if (keys.d_right)
+            x++;     
+        if (keys.d_left)
+            x--; 
     }
 }
--- src/filesystem/filesystem.h
+++ src/filesystem/filesystem.h
@@ -1,6 +1,9 @@
 extern unsigned char ERROR_IPL_YAZ0[];
 #define ERROR_IPL_YAZ0_LEN 23274
 
+extern unsigned char MARIOSPRITE_BIN[];
+#define MARIOSPRITE_BIN_LEN 85352
+
 extern unsigned char TITLE_YAZ0[];
 #define TITLE_YAZ0_LEN 21472
 
